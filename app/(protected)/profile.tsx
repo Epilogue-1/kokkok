6688136a6cbdac3bc73ef241e00a2e69f4ed9eb2
CitrusSoda <dndhkaus1004@naver.com>
2025-03-16 00:52:09 +0900

    refactor: 프로필 수정 디자인 변경경

--- app/(protected)/profile.tsx
+++ app/(protected)/profile.tsx
@@ -1,3 +1,4 @@
+import colors from "@/constants/colors";
 import Icons from "@/constants/icons";
 import useFetchData from "@/hooks/useFetchData";
 import { useModal } from "@/hooks/useModal";
@@ -8,12 +9,11 @@ import { useEffect, useState } from "react";
 import {
   Alert,
   Image,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
+  Keyboard,
   Text,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from "react-native";
 
@@ -62,72 +62,84 @@ const Profile = () => {
   };
 
   return (
-    <>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        className="h-full flex-1 bg-white"
-      >
-        <ScrollView>
-          <View className="mt-12 flex items-center justify-center px-6">
-            <TouchableOpacity
-              onPress={() => {
-                openModal({
-                  type: "SELECT_PROFILE_IMAGE_EDIT",
-                  setProfileInput: setProfileInput,
-                });
-              }}
-              className="relative"
-            >
-              <Image
-                source={
-                  profileInput.avatarUrl
-                    ? { uri: profileInput.avatarUrl }
-                    : images.AvatarInput
-                }
-                className="size-[236px] rounded-full"
-                resizeMode="cover"
-              />
-              <Icons.CameraIcon
-                style={{ position: "absolute", bottom: 12, right: 14 }}
-              />
-            </TouchableOpacity>
-
-            <View className="mt-10 flex w-full gap-10">
-              <TextInput
-                className="placeholder:body-1 h-[58px] w-full rounded-[10px] border border-gray-20 px-4 focus:border-primary"
-                placeholder="닉네임을 입력해주세요."
-                accessibilityLabel="닉네임 입력"
-                accessibilityHint="닉네임을 입력해주세요."
-                value={profileInput.username}
-                onChangeText={(text) =>
-                  setProfileInput({ ...profileInput, username: text })
-                }
-              />
-              <TextInput
-                className="placeholder:body-1 h-[108px] w-full rounded-[10px] border border-gray-20 p-4 focus:border-primary"
-                placeholder="소개글을 입력해주세요."
-                accessibilityLabel="소개글 입력"
-                accessibilityHint="소개글을 입력해주세요."
-                multiline={true} // 여러 줄 입력 가능
-                numberOfLines={4} // 기본 표시 줄 수
-                value={profileInput.description}
-                onChangeText={(text) =>
-                  setProfileInput({ ...profileInput, description: text })
-                }
-                textAlignVertical="top"
-              />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View className="relative flex-1 bg-white">
+        <View className="mt-12 flex items-center justify-center px-6">
+          <TouchableOpacity
+            onPress={() => {
+              openModal({
+                type: "SELECT_PROFILE_IMAGE_EDIT",
+                setProfileInput: setProfileInput,
+              });
+            }}
+            className="relative"
+          >
+            <Image
+              source={
+                profileInput.avatarUrl
+                  ? { uri: profileInput.avatarUrl }
+                  : images.AvatarInput
+              }
+              className="size-[236px] rounded-full"
+              resizeMode="cover"
+            />
+            <View className="absolute top-[176px] left-[174px] size-[48px] items-center justify-center rounded-full border-2 border-white bg-gray-25">
+              <Icons.CameraIcon width={24} height={24} />
             </View>
+          </TouchableOpacity>
 
+          <View className="mt-10 flex w-full">
+            <TextInput
+              className="title-3 h-[58px] w-full rounded-[10px] border border-gray-20 px-4 text-gray-90 focus:border-primary"
+              placeholder="닉네임을 입력해주세요."
+              placeholderTextColor={colors.gray[60]}
+              accessibilityLabel="닉네임 입력"
+              accessibilityHint="닉네임을 입력해주세요."
+              value={profileInput.username}
+              onChangeText={(text) =>
+                setProfileInput({ ...profileInput, username: text })
+              }
+            />
             <TouchableOpacity
-              className="mt-12 h-[62px] w-full items-center justify-center rounded-[10px] bg-primary"
-              onPress={handleEditProfile}
+              className="absolute top-[17px] right-[16px]"
+              onPress={() => setProfileInput({ ...profileInput, username: "" })}
             >
-              <Text className="heading-2 text-white">완료</Text>
+              <Icons.XIcon color={colors.gray[80]} />
             </TouchableOpacity>
+            <TextInput
+              className="body-5 mt-[24px] h-[132px] w-full rounded-[10px] border border-gray-20 p-4 text-gray-90 focus:border-primary"
+              placeholder="소개글을 입력해주세요."
+              placeholderTextColor={colors.gray[60]}
+              accessibilityLabel="소개글 입력"
+              accessibilityHint="소개글을 입력해주세요"
+              multiline={true} // 여러 줄 입력 가능
+              numberOfLines={4} // 기본 표시 줄 수
+              value={profileInput.description}
+              onChangeText={(text) =>
+                setProfileInput({ ...profileInput, description: text })
+              }
+              textAlignVertical="top"
+            />
+            <Text className="caption-2 p-[8px] text-gray-80">
+              ({profileInput.description.length}/100)
+            </Text>
           </View>
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </>
+        </View>
+        <View className="absolute right-0 bottom-[32px] left-0 px-6">
+          <TouchableOpacity
+            className={`h-[62px] w-full items-center justify-center rounded-[10px] ${
+              profileInput.description.length > 100
+                ? "bg-gray-40"
+                : "bg-primary"
+            }`}
+            onPress={handleEditProfile}
+            disabled={profileInput.description.length > 100}
+          >
+            <Text className="heading-2 text-white">완료</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
