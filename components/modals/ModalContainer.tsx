a2152044df33a5694487223adeeabd577b14df9e
wjsdncl <wjsdncl2222@gmail.com>
2025-03-23 22:30:44 +0900

    refactor: 모달 View를 Modal로 변경

--- components/modals/ModalContainer.tsx
+++ components/modals/ModalContainer.tsx
@@ -9,6 +9,7 @@ import {
   useRef,
   useState,
 } from "react";
+import { Modal } from "react-native";
 import {
   Animated,
   BackHandler,
@@ -90,18 +91,16 @@ const ModalItem = forwardRef<ModalItemRef, ModalItemProps>(
       }
     }, [isClosing, modal.position, fadeAnim, slideAnim, onClose]);
 
-    // 외부에서 handleClose() 호출할 수 있도록 ref에 노출
     useImperativeHandle(ref, () => ({
       handleClose,
     }));
 
     return (
-      <View className="absolute inset-0">
+      <Modal transparent animationType={"none"} className="absolute inset-0">
         <View
           className={`size-full flex-1 ${
             index === 0 ? "bg-black/50" : ""
           } ${modal.position === "center" ? "justify-center" : "justify-end"}`}
-          // 최상단 모달만 터치 시 닫힘 (exit 애니메이션 실행)
           onTouchStart={isTop ? handleClose : undefined}
         >
           <Animated.View
@@ -124,7 +123,7 @@ const ModalItem = forwardRef<ModalItemRef, ModalItemProps>(
             {modal.content}
           </Animated.View>
         </View>
-      </View>
+      </Modal>
     );
   },
 );
@@ -132,22 +131,19 @@ const ModalItem = forwardRef<ModalItemRef, ModalItemProps>(
 export default function ModalContainer(): JSX.Element | null {
   const [modalStack] = useAtom(modalStackAtom);
   const { closeModal } = useModal();
-  // 최상단 모달에 대한 ref 생성
   const topModalRef = useRef<ModalItemRef>(null);
 
-  // 하드웨어 뒤로가기 버튼 처리: 모달이 열려있으면 최상단 모달의 exit 애니메이션 실행
   useEffect(() => {
     const onBackPress = (): boolean => {
       if (modalStack.length > 0) {
         if (topModalRef.current) {
           topModalRef.current.handleClose();
         } else {
-          // 혹시 ref가 없을 경우 fallback으로 바로 닫기
           closeModal();
         }
-        return true; // 이벤트 소모
+        return true;
       }
-      return false; // 기본 동작 수행
+      return false;
     };
 
     const backHandler = BackHandler.addEventListener(
@@ -171,7 +167,6 @@ export default function ModalContainer(): JSX.Element | null {
             modal={modal}
             isTop={isTop}
             onClose={closeModal}
-            // 최상단 모달에 ref 전달
             ref={isTop ? topModalRef : null}
           />
         );
