bfa595065b28353bcd27cc271a18e48d4ee4a2c6
YehaYoo <151544448+YehaYoo@users.noreply.github.com>
2025-03-26 23:05:34 +0900

    refector: 친구 페이지 디자인 수정 (#189)
    * refactor: 친구 요청 페이지 디자인 변경

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -15,7 +15,7 @@ export default function SearchBar({
 }: SearchBarProps) {
   return (
     <View
-      className={`w-full h-[56px] rounded-[10px] bg-gray-10 flex-row ${customClassName ? customClassName : ""}`}
+      className={`w-full h-[54px] rounded-[10px] bg-gray-10 flex-row ${customClassName ? customClassName : ""}`}
     >
       <icons.SearchIcon
         width={24}
@@ -29,8 +29,8 @@ export default function SearchBar({
         onChangeText={(e) => handleChangeText(e)}
         // onSubmitEditing={() => {}} todo
         returnKeyType="search"
-        placeholder="친구 검색"
-        className="body-1 w-full placeholder:text-gray-45 text-gray-90 p-0"
+        placeholder="닉네임을 입력해주세요"
+        className="pt-[2px] body-2 w-full placeholder:text-gray-60 text-gray-100 p-0"
         accessibilityRole="search"
         accessibilityLabel="친구 검색"
         accessibilityHint="친구를 검색하려면 이름을 입력하세요"
