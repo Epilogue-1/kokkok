3f1116ed8d8528fce80e1b56a071adc7590936d5
CitrusSoda <dndhkaus1004@naver.com>
2025-05-11 18:28:24 +0900

    feat: 마이페이지 및 유저페이지 ui 변경

--- components/PostGrid.tsx
+++ components/PostGrid.tsx
@@ -41,7 +41,7 @@ export default function PostGrid({ refetch, posts, isError }: PostGridProps) {
 
   if (isError) {
     return (
-      <View className="mt-8 flex-1 items-center justify-center bg-gray-5">
+      <View className="mt-8 flex-1 items-center justify-center border-[3px] border-white bg-gray-5">
         <Image
           source={images.ErrorPost}
           className="h-[178px] w-[234px]"
@@ -55,7 +55,7 @@ export default function PostGrid({ refetch, posts, isError }: PostGridProps) {
 
   if (!posts || posts.length === 0) {
     return (
-      <View className="mt-8 flex-1 items-center justify-center bg-gray-5">
+      <View className="mt-8 flex-1 items-center justify-center border-[3px] border-white bg-gray-5">
         <Image
           source={images.NoPost}
           className="h-[178px] w-[234px]"
@@ -68,28 +68,39 @@ export default function PostGrid({ refetch, posts, isError }: PostGridProps) {
   }
 
   return (
-    <View className="mt-[32px] flex-1 bg-gray-5">
-      <FlatList
-        ref={flatListRef}
-        data={posts}
-        renderItem={({ item }) => {
-          const size = Dimensions.get("window").width / 3;
-          return (
-            <View style={{ height: size, width: size }}>
-              <TouchableOpacity onPress={() => router.push(`/post/${item.id}`)}>
-                <Image
-                  source={{ uri: item.images[0] }}
-                  resizeMode="cover"
-                  style={{ width: "100%", height: "100%" }}
-                  defaultSource={images.ErrorPost}
-                />
-              </TouchableOpacity>
-            </View>
-          );
-        }}
-        numColumns={3}
-        keyExtractor={(item) => item.id.toString()}
-      />
+    <View className="mt-[48px] flex-1">
+      <View className="overflow-hidden rounded-xl border-[3px] border-white bg-gray-5">
+        <FlatList
+          ref={flatListRef}
+          data={posts}
+          renderItem={({ item }) => {
+            const size = Dimensions.get("window").width / 3;
+            return (
+              <View
+                style={{
+                  height: size,
+                  width: size,
+                  marginRight: 3,
+                  marginBottom: 3,
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => router.push(`/post/${item.id}`)}
+                >
+                  <Image
+                    source={{ uri: item.images[0] }}
+                    resizeMode="cover"
+                    style={{ width: "100%", height: "100%" }}
+                    defaultSource={images.ErrorPost}
+                  />
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+          numColumns={3}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
     </View>
   );
 }
