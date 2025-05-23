28f5436ddcf7b91be65222535d7865ce13a1183c
heony704 <heony704@gmail.com>
2025-05-23 18:02:36 +0900

    package 랑 metro 빼고 다나가

--- components/Skeleton/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Skeleton } from "./Skeleton";
-export { default as CalendarSkeleton } from "./CalendarSkeleton";
-export { default as CommentSkeleton } from "./CommentSkeleton";
--- constants/time.ts
+++
@@ -1,2 +0,0 @@
-export const OTP_TIME = 3600; // s
-export const POKE_TIME = 3600000; // ms
